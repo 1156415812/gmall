6e30b33c5bb8215ff6c1edde5b64f9e410c00a4d
1156415812 <1156415812@qq.com>
2020-02-26 22:43:56 +0800

    111

---
+++ gmall-pms/src/test/java/com/deep/gmall/pms/GmallPmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deep.gmall.pms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallPmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
